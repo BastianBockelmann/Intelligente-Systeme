e0f936d91b94d2806cb8f7056c6c534284d09b00
Basti B <bastian.bockelmann@hotmail.de>
2024-09-16 19:12:07 +0200

    FIX Layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/devtools',
+    '@nuxt/ui'
+  ],
   devtools: { enabled: true },
   typescript: {
     shim: false
